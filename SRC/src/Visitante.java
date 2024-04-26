117f736f51ae88326598601af88478ad17b3e0ad
Tomáz-m-feijo <Tomazzmaciel@gmail.com>
2024-04-26 18:16:56 -0300

    Adicionada lista de atraçoes para armazenar quais o visitante foi

--- SRC/src/Visitante.java
+++ SRC/src/Visitante.java
@@ -1,8 +1,12 @@
+import java.util.ArrayList;
+
 public abstract class Visitante {
 
 	private int Nome;
 
 	private int Ano_nascimento;
+
+	private ArrayList<Atracao> atracoes_visitadas;
 
 	public void visitante(int Nome, int ano) {
 
@@ -11,7 +15,9 @@ public abstract class Visitante {
 	/**
 	 * calcula a idade baseado no ano de nascimento
 	 */
-	public int GetIdade(){return 0;}
+	public int GetIdade() {
+		return 2024 - Ano_nascimento; //calculo temporario de idade }
+	}
 
 	public abstract double CalculaPreco();
 
