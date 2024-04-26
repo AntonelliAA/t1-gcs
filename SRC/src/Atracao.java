c2673ee45a71099a7d9fa3d6b895af62eca0d80e
EDUARDO CAMANA <e.camana@edu.pucrs.br>
2024-04-26 18:39:13 -0300

    Construtor

--- SRC/src/Atracao.java
+++ SRC/src/Atracao.java
@@ -1,3 +1,13 @@
 public class Atracao {
-    private  String nome;
+    private String nome;
+    private int capacidade;
+    private int ingressos;
+    private double faturameto;
+
+    public Atracao(String nome, int capacidade, int ingressos, double faturameto) {
+        this.nome = nome;
+        this.capacidade = capacidade;
+        this.ingressos = ingressos;
+        this.faturameto = faturameto;
+    }
 }
