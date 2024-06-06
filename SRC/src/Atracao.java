64d141d80952969b53d9675afe3330c50e46c50a
Eduardo Ferrari <eduferrari.du@gmail.com>
2024-06-06 15:37:13 -0300

    Adiconando atracões padrões, quando parque iniciar irá começar com elas.

--- SRC/src/Atracao.java
+++ SRC/src/Atracao.java
@@ -1,13 +1,31 @@
 public class Atracao {
     private String nome;
     private int capacidade;
-    private int ingressos;
-    private double faturameto;
 
-    public Atracao(String nome, int capacidade, int ingressos, double faturameto) {
+
+    public Atracao(String nome, int capacidade) {
         this.nome = nome;
         this.capacidade = capacidade;
-        this.ingressos = ingressos;
-        this.faturameto = faturameto;
+
     }
+
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getCapacidade() {
+        return capacidade;
+    }
+
+    public void setCapacidade(int capacidade) {
+        this.capacidade = capacidade;
+    }
+
+
+
 }
--- SRC/src/Parque.java
+++ SRC/src/Parque.java
@@ -14,12 +14,29 @@ public class Parque {
 		visitantes = new ArrayList<>();
 		atracoes = new ArrayList<>();
 		isClosed = false;
+		atracoesPadrao();
 	}
 
 	public void Registra_visitante() {
 	//TODO adicionar diferenciação para adulto e criança, adicionando os parametros adicionais
 	}
-	public void Registra_atracoes() {
+	public void Registra_atracoes(Atracao a) {
+		atracoes.add(a);
+	}
+
+	public void atracoesPadrao () {
+		Atracao a1 = new Atracao("Roda-Gigante", 15);
+		Atracao a2 = new Atracao("Casa do Terror", 40);
+		Atracao a3 = new Atracao("Barco Viking", 60);
+		Atracao a4 = new Atracao("Carrinho de Bate-Bate", 10);
+		Atracao a5 = new Atracao("Trem Fantasma", 20);
+		Atracao a6 = new Atracao("Tirolesa", 2);
+		atracoes.add(a1);
+		atracoes.add(a2);
+		atracoes.add(a3);
+		atracoes.add(a4);
+		atracoes.add(a5);
+		atracoes.add(a6);
 
 	}
 
